7e374b112fd3e7e6ea346131c40e17c7254f64bf
Arne Jørgensen <arne@arnested.dk>
2020-12-05 19:38:01 +0100

    fix: Fix problem matcher (#14)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM hadolint/hadolint:v1.17.5-alpine
 
-COPY LICENSE README.md /
+COPY LICENSE README.md problem-matcher.json /
 COPY hadolint.sh /usr/local/bin/hadolint.sh
 
 ENTRYPOINT [ "/usr/local/bin/hadolint.sh" ]
--- hadolint.sh
+++ hadolint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-echo '::add-matcher::problem-matcher.json'
+# The problem-matcher definition must be present in the repository
+# checkout (outside the Docker container running hadolint). We create
+# a temporary folder and copy problem-matcher.json to it and make it
+# readable.
+TMP_FOLDER=$(mktemp -d -p .)
+cp /problem-matcher.json "${TMP_FOLDER}"
+chmod -R a+rX "${TMP_FOLDER}"
+trap "rm -rf \"${TMP_FOLDER}\"" EXIT
+
+echo "::add-matcher::${TMP_FOLDER}/problem-matcher.json"
 
 hadolint "$@"
