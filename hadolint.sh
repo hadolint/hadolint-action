138ff5476e79fcfc65725f1abc565b6aa6c09d60
Arne Jørgensen <arne@arnested.dk>
2020-12-06 12:25:55 +0100

    Remove problem matcher after run

--- hadolint.sh
+++ hadolint.sh
@@ -7,7 +7,15 @@
 TMP_FOLDER=$(mktemp -d -p .)
 cp /problem-matcher.json "${TMP_FOLDER}"
 chmod -R a+rX "${TMP_FOLDER}"
-trap "rm -rf \"${TMP_FOLDER}\"" EXIT
+
+# After the run has finished we remove the problem-matcher.json from
+# the repository so we don't leave the checkout dirty. We also remove
+# the matcher so it won't take effect in later steps.
+cleanup() {
+    echo "::remove-matcher owner=brpaz/hadolint-action::"
+    rm -rf "${TMP_FOLDER}"
+}
+trap cleanup EXIT
 
 echo "::add-matcher::${TMP_FOLDER}/problem-matcher.json"
 
