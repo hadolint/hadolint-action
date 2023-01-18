a4d0f06c93b55ad8a6f376988283d88cb988178c
José Lorenzo Rodríguez <lorenzo@users.noreply.github.com>
2023-01-18 19:22:00 +0100

    Revert "Some `shellcheck` fixes and new input to print Hadolint results to console"

--- hadolint.sh
+++ hadolint.sh
@@ -3,10 +3,7 @@
 # The problem-matcher definition must be present in the repository
 # checkout (outside the Docker container running hadolint). We copy
 # problem-matcher.json to the home folder.
-PROBLEM_MATCHER_FILE="/problem-matcher.json"
-if [ -f "$PROBLEM_MATCHER_FILE" ]; then
-    cp "$PROBLEM_MATCHER_FILE" "$HOME/"
-fi
+cp /problem-matcher.json "$HOME/"
 
 # After the run has finished we remove the problem-matcher.json from
 # the repository so we don't leave the checkout dirty. We also remove
@@ -26,18 +23,16 @@ if [ -z "$HADOLINT_TRUSTED_REGISTRIES" ]; then
   unset HADOLINT_TRUSTED_REGISTRIES;
 fi
 
-COMMAND="hadolint $HADOLINT_CONFIG"
-
 if [ "$HADOLINT_RECURSIVE" = "true" ]; then
   shopt -s globstar
 
   filename="${!#}"
   flags="${@:1:$#-1}"
 
-  RESULTS=$(eval "$COMMAND $flags" -- **/$filename)
+  RESULTS=$(hadolint $HADOLINT_CONFIG $flags **/$filename)
 else
-
-  RESULTS=$(eval "$COMMAND" "$@")
+  # shellcheck disable=SC2086
+  RESULTS=$(hadolint $HADOLINT_CONFIG "$@")
 fi
 FAILED=$?
 
@@ -45,18 +40,16 @@ if [ -n "$HADOLINT_OUTPUT" ]; then
   if [ -f "$HADOLINT_OUTPUT" ]; then
     HADOLINT_OUTPUT="$TMP_FOLDER/$HADOLINT_OUTPUT"
   fi
-  echo "$RESULTS" > "$HADOLINT_OUTPUT"
-fi
-
-if [ "$HADOLINT_RESULTS_TO_CONSOLE" = "true" ]; then
-    echo "$RESULTS"
+  echo "$RESULTS" > $HADOLINT_OUTPUT
 fi
 
 RESULTS="${RESULTS//$'\\n'/''}"
 
-{ echo "results<<EOF"; echo "$RESULTS"; echo "EOF"; } >> "$GITHUB_OUTPUT"
+echo "results<<EOF" >> $GITHUB_OUTPUT
+echo "${RESULTS}" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
 
-{ echo "HADOLINT_RESULTS<<EOF"; echo "$RESULTS"; echo "EOF"; } >> "$GITHUB_ENV"
+{ echo "HADOLINT_RESULTS<<EOF"; echo "$RESULTS"; echo "EOF"; } >> $GITHUB_ENV
 
 [ -z "$HADOLINT_OUTPUT" ] || echo "Hadolint output saved to: $HADOLINT_OUTPUT"
 
