a17195f03eccfa4994f0a3c2b53175d4b70de259
Viacheslav Kudinov <viacheslav.kudinov@gmail.com>
2023-01-19 20:09:05 +0100

    Fix of ShellCheck and doc update. Int testing updates. Added ShellCheck action. (#1)

--- hadolint.sh
+++ hadolint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
 # The problem-matcher definition must be present in the repository
 # checkout (outside the Docker container running hadolint). We copy
 # problem-matcher.json to the home folder.
-cp /problem-matcher.json "$HOME/"
 
+PROBLEM_MATCHER_FILE="/problem-matcher.json"
+if [ -f "$PROBLEM_MATCHER_FILE" ]; then
+    cp "$PROBLEM_MATCHER_FILE" "$HOME/"
+fi
 # After the run has finished we remove the problem-matcher.json from
 # the repository so we don't leave the checkout dirty. We also remove
 # the matcher so it won't take effect in later steps.
+# shellcheck disable=SC2317
 cleanup() {
     echo "::remove-matcher owner=brpaz/hadolint-action::"
 }
@@ -23,16 +26,19 @@ if [ -z "$HADOLINT_TRUSTED_REGISTRIES" ]; then
   unset HADOLINT_TRUSTED_REGISTRIES;
 fi
 
+COMMAND="hadolint $HADOLINT_CONFIG"
+
 if [ "$HADOLINT_RECURSIVE" = "true" ]; then
   shopt -s globstar
 
   filename="${!#}"
-  flags="${@:1:$#-1}"
 
-  RESULTS=$(hadolint $HADOLINT_CONFIG $flags **/$filename)
+  flags="${*:1:$#-1}"
+
+  RESULTS=$(eval "$COMMAND $flags" -- **/"$filename")
 else
-  # shellcheck disable=SC2086
-  RESULTS=$(hadolint $HADOLINT_CONFIG "$@")
+  flags=$*
+  RESULTS=$(eval "$COMMAND" "$flags")
 fi
 FAILED=$?
 
@@ -40,16 +46,14 @@ if [ -n "$HADOLINT_OUTPUT" ]; then
   if [ -f "$HADOLINT_OUTPUT" ]; then
     HADOLINT_OUTPUT="$TMP_FOLDER/$HADOLINT_OUTPUT"
   fi
-  echo "$RESULTS" > $HADOLINT_OUTPUT
+  echo "$RESULTS" > "$HADOLINT_OUTPUT"
 fi
 
 RESULTS="${RESULTS//$'\\n'/''}"
 
-echo "results<<EOF" >> $GITHUB_OUTPUT
-echo "${RESULTS}" >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
+{ echo "results<<EOF"; echo "$RESULTS"; echo "EOF"; } >> "$GITHUB_OUTPUT"
 
-{ echo "HADOLINT_RESULTS<<EOF"; echo "$RESULTS"; echo "EOF"; } >> $GITHUB_ENV
+{ echo "HADOLINT_RESULTS<<EOF"; echo "$RESULTS"; echo "EOF"; } >> "$GITHUB_ENV"
 
 [ -z "$HADOLINT_OUTPUT" ] || echo "Hadolint output saved to: $HADOLINT_OUTPUT"
 
